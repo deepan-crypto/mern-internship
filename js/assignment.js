34d18530374290033b6288088dd3f35e59c45bd8
deepan-crypto <deepang467@gmail.com>
2025-12-04 12:05:41 +0530

    q2

--- js/assignment.js
+++ js/assignment.js
@@ -5,3 +5,12 @@ function createArray(arg1, arg2, arg3) {
 
 const resultQ1 = createArray(10, "hello", true);
 console.log(resultQ1); 
+
+//q2
+function stringOnly(...args) {
+  return args.filter(arg => typeof arg === 'string');
+}
+
+
+const resultQ2 = stringOnly(1, "apple", true, "banana", 42, "cherry");
+console.log(resultQ2); 
