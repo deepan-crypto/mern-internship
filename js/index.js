d4d62b8a60677dfb0367f099e44c6e0deb793027
deepan-crypto <deepang467@gmail.com>
2025-12-04 10:06:35 +0530

    updated  index.js

---
+++ js/index.js
@@ -0,0 +1,27 @@
+console.log(a);
+//hoistng
+var a = 10;
+
+/global scope / Function scope
+console.log(a);
+
+function test() 
+//ES6 
+// let ans const
+//console.log(b); //ReferenceError
+// temporal dead zone
+
+let b = 20;
+b=130;
+
+//Block scope
+
+console.log(b);
+
+// { let b==30 // console.log(b); }
+
+console.log(c);
+
+cosnt c=40;
+c=70;
+consolre.log(c);
