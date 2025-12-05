daa612a23610217c314596edb30b013b2e343845
deepan-crypto <deepang467@gmail.com>
2025-12-05 12:16:38 +0530

    index.js

---
+++ dom-assignment/index.js
@@ -0,0 +1,11 @@
+
+let textPara = document.getElementById("text");
+let box = document.getElementById("box");
+let itemInput = document.getElementById("itemInput");
+let list = document.getElementById("list");
+
+let btnChangeText = document.querySelector('button[onclick="changeText()"]');
+let btnChangeColor = document.querySelector('button[onclick="changeColor()"]');
+let btnHide = document.querySelector('button[onclick="hideBox()"]');
+let btnShow = document.querySelector('button[onclick="showBox()"]');
+let btnAddItem = document.querySelector('button[onclick="addItem()"]');
