1264a48b6337cbde6b2ef71acad975d5b10cd5d1
deepan-crypto <deepang467@gmail.com>
2025-12-05 12:16:56 +0530

    index.js

--- dom-assignment/index.js
+++ dom-assignment/index.js
@@ -9,3 +9,13 @@ let btnChangeColor = document.querySelector('button[onclick="changeColor()"]');
 let btnHide = document.querySelector('button[onclick="hideBox()"]');
 let btnShow = document.querySelector('button[onclick="showBox()"]');
 let btnAddItem = document.querySelector('button[onclick="addItem()"]');
+
+
+
+
+btnChangeText.addEventListener("click", changeText);
+btnChangeColor.addEventListener("click", changeColor);
+btnHide.addEventListener("click", hideBox);
+btnShow.addEventListener("click", showBox);
+btnAddItem.addEventListener("click", addItem);
+
