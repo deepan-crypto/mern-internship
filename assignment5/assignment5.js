03ab82dd72eca7a01b0a8bb26a46d6de61faaed3
deepan-crypto <deepang467@gmail.com>
2025-12-05 11:21:38 +0530

    update

--- assignment5/assignment5.js
+++ assignment5/assignment5.js
@@ -1,9 +1,25 @@
-//All instances from assignment5.js
+// 1 We need all the instance
+// 2. Generate a Rnadom Number betwenn 1 and 20
+// 2.1 creat a varible of score
+// Button Functionalities
 
 let againBtn = document.querySelector(".again");
-let checkBtn = document.querySelector(".left button");
-let inputField = document.querySelector(".left input");
-let messagePara = document.querySelector(".right p");
-let scorePara = document.querySelector(".second p:nth-child(1)");
-let highScorePara = document.querySelector(".second p:nth-child(2)");
-let markBox = document.querySelector(".mark");
+let number = document.querySelector(".number");
+let guess = document.querySelector(".guess");
+let checkBtn = document.querySelector(".check");
+let score = document.querySelector(".score");
+let highScore = document.querySelector(".highscore");
+let msg = document.querySelector(".message");
+
+// 2. Generate a Rnadom Number betwenn 1 and 20
+let randomNumber = Math.trunc(Math.random() * 20) + 1;
+
+// 2.1 creat a varible of score
+let scr = 20;
+
+// Button Functionalities
+// 1.event  that we want to occur 2. Callback
+checkBtn.addEventListener("click", () => {
+  console.log("Button Clicked", randomNumber);
+  console.log(randomNumber);
+});
