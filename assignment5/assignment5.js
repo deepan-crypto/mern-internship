1ab65747ef6c9a48dc43b92f5c8dc0c8e8fe4c97
deepan-crypto <deepang467@gmail.com>
2025-12-05 12:00:41 +0530

    index.html

--- assignment5/assignment5.js
+++ assignment5/assignment5.js
@@ -1,8 +1,4 @@
 // 1 We need all the instance
-// 2. Generate a Rnadom Number betwenn 1 and 20
-// 2.1 creat a varible of score
-// Button Functionalities
-
 let againBtn = document.querySelector(".again");
 let number = document.querySelector(".number");
 let guess = document.querySelector(".guess");
@@ -21,5 +17,42 @@ let scr = 20;
 // 1.event  that we want to occur 2. Callback
 checkBtn.addEventListener("click", () => {
   console.log("Button Clicked", randomNumber);
-  console.log(randomNumber);
+  let inputVal = Number(guess.value);
+  if (!inputVal) {
+    msg.textContent = "No Number!";
+  } else if (randomNumber === inputVal) {
+    console.log("Value is correct");
+    number.textContent = randomNumber;
+    msg.textContent = "Correct value!";
+    highScore.textContent = scr;
+    document.body.style.backgroundColor = "green";
+  } else if (randomNumber < inputVal) {
+    console.log("Value is too high");
+    scr -= 1;
+    msg.textContent = "Too high!";
+  } else {
+    console.log("Value is too low");
+    scr -= 1;
+    msg.textContent = "Too low!";
+  }
 });
+againBtn.addEventListener("click", () => {
+  msg.textContent = "Start guessing..";
+  document.body.style.backgroundColor = "black";
+  number.textContent = "?";
+  scr = 20;
+});
+
+
+//4. again button functionality
+let init=()=>{
+  scr=20;
+   score.textContent=scr;
+   number.textContent="?";
+   guess.value="";
+   msg.textContent="Start guessing..";
+   randomNumber=Math.trunc(Math.random()*20)+1;
+
+   document.body.style.backgroundColor="black";
+}
+againBtn.
