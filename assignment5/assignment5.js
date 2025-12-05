1e3a4df9a221a141feff9bc8cc43b59e0c6200f2
deepan-crypto <deepang467@gmail.com>
2025-12-05 11:10:51 +0530

    assignment5.js

---
+++ assignment5/assignment5.js
@@ -0,0 +1,9 @@
+//All instances from assignment5.js
+
+let againBtn = document.querySelector(".again");
+let checkBtn = document.querySelector(".left button");
+let inputField = document.querySelector(".left input");
+let messagePara = document.querySelector(".right p");
+let scorePara = document.querySelector(".second p:nth-child(1)");
+let highScorePara = document.querySelector(".second p:nth-child(2)");
+let markBox = document.querySelector(".mark");
