e106c2b56200fe403869844ec484fa2822e6fecf
deepan-crypto <deepang467@gmail.com>
2025-12-08 12:07:42 +0530

    promise

--- async/async.js
+++ async/async.js
@@ -1,18 +1,49 @@
-setTimeout(()=>{
-    console.log("Set Timeout Function");
-},2000);
+// setTimeout(()=>{
+//     console.log("Set Timeout Function");
+// },2000);
 
-//CAll back hell
-// nested callback
+// //CAll back hell
+// // nested callback
 
-setTimeout(()=>{
-    console.log("First Timeout");
-    setTimeout(()=>{
-        console.log("Second Timeout");
-        setTimeout(()=>{
-          //  return; to s=s make an error );
-        },2000);
-    },2000);
-},2000);    
+// setTimeout(()=>{
+//     console.log("First Timeout");
+//     setTimeout(()=>{
+//         console.log("Second Timeout");
+//         setTimeout(()=>{
+//           //  return; to s=s make an error );
+//         },2000);
+//     },2000);
+// },2000);    
 
+
+//Promise
+//3 states
+//pending       
+//fulfilled
+//rejected
+
+let mypromise=new Promise((res,rej)=>{
+            let mark=60;
+            if(marks >= 60){
+                res("Student passed");
+            }
+            else{
+                rej("Student failed");
+            }
+
+});
+
+// ways to ca;ll promise
+//.then()
+// .catch()
+
+ 
+// promise pass a callback function
+ mypromise
+    .then((a)=>{
+    console.log(a);
+ })
+ .catch((err)=>{
+    console.log(err);
+ });
 
