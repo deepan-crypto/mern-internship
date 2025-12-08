f1f8b6cad14951c723e5a44024683c5152b4ead5
deepan-crypto <deepang467@gmail.com>
2025-12-08 11:24:38 +0530

    async.js

--- async/async.js
+++ async/async.js
@@ -2,3 +2,15 @@ setTimeout(()=>{
     console.log("Set Timeout Function");
 },2000);
 
+//CAll back hell
+// nested callback
+
+setTimeout(()=>{
+    console.log("First Timeout");
+    setTimeout(()=>{
+        console.log("Second Timeout");
+        setTimeout(()=>{
+            console.log("Third Timeout");
+        },2000);
+    },2000);
+},2000);    
