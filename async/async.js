23ab1cae0be110772a213a23c8458146a200a671
deepan-crypto <deepang467@gmail.com>
2025-12-08 12:51:26 +0530

    fetch data

--- async/async.js
+++ async/async.js
@@ -93,6 +93,7 @@ let fetchData=async()=>{
       const response=await fetch("https://jsonplaceholder.typicode.com/todos");
       console.log(response);
         const data=await response.json();
+        localStorage.setItem("responseDATA",JSON.stringify(data));
         console.log("value is",data);
     }
     catch(error){
