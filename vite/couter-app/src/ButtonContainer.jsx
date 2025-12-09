c065bdae7fcf683be46c434c3d9ab72d31a268a8
deepan-crypto <deepang467@gmail.com>
2025-12-09 12:09:42 +0530

    ButtonContainer

---
+++ vite/couter-app/src/ButtonContainer.jsx
@@ -0,0 +1,16 @@
+import Button from "./Button";
+
+const ButtonContainer = ({ handleIncrement, handleDecrement }) => {
+  return (
+    <div className="Button">
+      <Button className="inc" onClick={handleIncrement}>
+        Increment
+      </Button>
+      <Button className="dec" onClick={handleDecrement}>
+        Decrement
+      </Button>
+    </div>
+  );
+};
+
+export default ButtonContainer;
