00cca750f6923b2ecf3e925821921a0321277317
deepan-crypto <deepang467@gmail.com>
2025-12-09 13:53:47 +0530

    app.jsx

--- vite/couter-app/src/App.jsx
+++ vite/couter-app/src/App.jsx
@@ -1,37 +1,31 @@
 import "./App.css";
 import { useState } from "react";
 import Counter from "./Counter";
-// import { Component1 } from "./Component1";
-// import Component2, { Component, Component3 } from "./Component2";
-// import Component4 from "./Component4";
-// import Component5 from "./Component5";
-// import NamedComponent, {
-//   NamedComponent2,
-//   NamedComponent3,
-// } from "./NamedComponent";
-// import GrandFather from "./GrandFather";
 
 function App() {
-  // let val = 0;
-  let [val, setVal] = useState(10);
-  const handleIncrement = () => {
-    // val = val + 1;
-    setVal(val + 1);
-    console.log("Increment Button Clickeed", val);
-  };
-  const handleDecrement = () => {
-    // val = val - 1;
-    setVal(val - 1);
-    console.log("Decrement Button Clickeed", val);
-  };
   return (
-    <div className="App">
-      <h1>Learning React</h1>
-      <Counter
-        val={val}
-        handleIncrement={handleIncrement}
-        handleDecrement={handleDecrement}
-      />
+    <div>
+      <header>
+        <h1>Guess My Number!</h1>
+        <p className="between">(Between 1 and 20)</p>
+        <button className="btn again">Again!</button>
+        <div className="number">?</div>
+      </header>
+      <main>
+        <section className="left">
+          <input type="number" className="guess" />
+          <button className="btn check">Check!</button>
+        </section>
+        <section className="right">
+          <p className="message">Start guessing...</p>
+          <p className="label-score">
+            💯 Score: <span className="score">20</span>
+          </p>
+          <p className="label-highscore">
+            🥇 Highscore: <span className="highscore">0</span>
+          </p>
+        </section>
+      </main>
     </div>
   );
 }
